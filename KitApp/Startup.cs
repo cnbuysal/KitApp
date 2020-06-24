f396173372e38f54918ed8275abd283a0784ddc7
Canberk Uysal <cnbuysalgit config --global user.email cnbuysal@gmail.com>
2020-06-24 17:33:15 +0300

    Uptaded

--- KitApp/Startup.cs
+++ KitApp/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using KitApp.Data;
 using KitApp.Data.Interfaces;
-using KitApp.Data.Mocks;
 using KitApp.Data.Repositories;
 using KitApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
